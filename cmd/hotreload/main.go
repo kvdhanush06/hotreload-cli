fc9a1b1cb5e5e8e3ec5282cf488d4a83921d9faa
Kakarlamudi Venkata Dhanush <kvdhanush06@hotmail.com>
2026-03-08 09:47:18 +0530

    feat: integrate watcher and runner via main event loop

--- cmd/hotreload/main.go
+++ cmd/hotreload/main.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"hotreload/internal/runner"
+	"hotreload/internal/watcher"
 )
 
 func main() {
@@ -31,4 +36,45 @@ func main() {
 	}
 
 	slog.Info("Starting hotreload...", "root", rootDir)
+
+	procRunner := runner.NewRunner(buildCmd, runCmd)
+
+	triggerChan := make(chan struct{}, 1)
+
+	onFileChange := func() {
+		select {
+		case triggerChan <- struct{}{}:
+			// Signal sent successfully
+		default:
+		}
+	}
+
+	fsWatcher, err := watcher.New(rootDir, onFileChange)
+	if err != nil {
+		slog.Error("Failed to initialize watcher", "error", err)
+		os.Exit(1)
+	}
+
+	if err := fsWatcher.Start(); err != nil {
+		slog.Error("Failed to start listening to file events", "error", err)
+		os.Exit(1)
+	}
+	defer fsWatcher.Stop()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	slog.Info("Triggering initial build...")
+	onFileChange()
+
+
+	for {
+		select {
+		case <-triggerChan:
+			procRunner.Restart()
+		case sig := <-sigChan:
+			slog.Info("Received stop signal", "signal", sig)
+			os.Exit(0)
+		}
+	}
 }
